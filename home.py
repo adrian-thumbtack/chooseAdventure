2c58ca441dd94218b6f306ef4800a3e97c3f08e7
Steven Liu <sirmrguy1153@gmail.com>
2018-02-25 22:22:59 -0500

    misc changes

--- home.py
+++ home.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-import tkinter as tk
+import Tkinter as tk
 from random import randint
 from Bestiary import *
 import math
@@ -254,7 +254,7 @@ def newRoom():
 def endGame():
     '''Displays green text on black background describing the end result, either "Game Over" or "You win!". This function only activates if the Boolean variable game has a value of false'''
     txt = None
-    if pl.hp == 0:          #If player is dead, game is over
+    if pl.hp <= 0:          #If player is dead, game is over
         txt = "Game Over!"
     else:                   #Otherwise, you win!!!
         txt = "You win!" 
@@ -421,7 +421,7 @@ def stuffHappens(jeff):
                 enPos[i].updatePos(newX, newY)
                 drawEnemy(enPos[i][0], enPos[i][1], i)
 
-        if pl.inv[5]: pl.hp = min(pl.maxhp,pl.hp+1)
+        if pl.inv[5] and len(enPos) > 0: pl.hp = min(pl.maxhp,pl.hp+1)
         
         updateStats()
     if not game:    #if game is false, end the game
